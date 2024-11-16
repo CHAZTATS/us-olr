bcd527ef5b4ed01d1c799b13f0026dba35fe3b0c
Charles Taverner <charles@ads4earth.com>
2024-11-16 17:32:35 +0000

    envs

--- config/environment/environment.prod.ts
+++ config/environment/environment.prod.ts
@@ -1,8 +1,6 @@
+import { environmentBase } from "./environment.base";
+
 export const environment = {
+    ...environmentBase,
     name: 'Prod',
-    production: true,
-    requestAction: 'OLR',
-    requestSource: 'DandGUS',
-    recaptchaSiteKey: '6Lff8MAZAAAAABoVsbvcKunIcyQydc9rVjCV5NZ7',
-    apiKey: 'HZRqLnZPGZ2QTgifn0Yw29ykai2g8f3h1bqh9E16'
 };
--- config/environment/environment.ts
+++ config/environment/environment.ts
@@ -1,11 +1,5 @@
+import { environmentBase } from "./environment.base";
+
 export const environment = {
-    name: 'Local',
-    production: false,
-    requestAction: 'OLR',
-    requestSource: 'DandGUS',
-    recaptchaSiteKey: '6Lff8MAZAAAAABoVsbvcKunIcyQydc9rVjCV5NZ7',
-    apiKey: 'HZRqLnZPGZ2QTgifn0Yw29ykai2g8f3h1bqh9E16',
-    addressyApiPrefix: 'https://api.addressy.com/Capture/Interactive',
-    addressyApiSuffix: '/v1.1/json3.ws',
-    addressyKey: 'ZG99-WD62-FP46-DC55'
+    ...environmentBase,
 };
